839bddbb2bb56d4ff77ead9d7536ccefac9487ee
Aneesh Dalvi <aneeshdalvi@MacBookPro.home>
2026-03-07 21:56:29 -0600

    implemented auth

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,10 +1,11 @@
 import { pgTable, serial, varchar, timestamp } from 'drizzle-orm/pg-core';
 
-export const user = pgTable('users', {
+export const users = pgTable('users', {
     id: serial('id').primaryKey(),
     name: varchar('name', { length: 255 }).notNull(),
     email: varchar('email', { length: 255 }).notNull().unique(),
-    password: varchar('password', { length: 50 }).notNull().default('user'),
+    password: varchar('password', { length: 255 }).notNull(),
+    role: varchar('role', { length: 50 }).notNull().default('user'),
     createdAt: timestamp('created_at').defaultNow().notNull(),
     updatedAt: timestamp('updated_at').defaultNow().notNull(),
 })
